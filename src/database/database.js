12a04c33f694d61a8896fd4a518409702ac1b82c
Andres <andres52885241@gmail.com>
2024-07-29 18:33:11 -0500

    some changes done for the database

--- src/database/database.js
+++ src/database/database.js
@@ -6,9 +6,10 @@ class Database {
     constructor() {
         this.conn = mysql.createConnection({
             host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            database: process.env.DB_NAME,
             user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
+            password: process.env.DB_PASSWORD
         });
 
         this.conn.connect(err => { if (err) console.error('Error al conectar a la base de datos: ' + err.stack) });
