804c043905482514b84a3eb35dd755953c71e342
Andres <andres52885241@gmail.com>
2024-07-29 18:05:32 -0500

    enviroment variables added

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
 
 class Database {
     constructor() {
         this.conn = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'ac-computers'
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
         });
 
         this.conn.connect(err => { if (err) console.error('Error al conectar a la base de datos: ' + err.stack) });
