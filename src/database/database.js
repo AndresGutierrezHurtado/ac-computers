1e318f3da3e0fcfd8656dd0bdb0675d92c46a06b
Andres <andres52885241@gmail.com>
2024-07-29 19:04:24 -0500

    fix: a mysql connection change done

--- src/database/database.js
+++ src/database/database.js
@@ -7,7 +7,7 @@ class Database {
             host: process.env.DB_HOST,
             port: process.env.DB_PORT,
             database: process.env.DB_NAME,
-            user: process.env.DB_USER,
+            user: process.env.BD_USER,
             password: process.env.DB_PASSWORD
         });
 
