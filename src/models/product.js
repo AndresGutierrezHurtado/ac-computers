e4342e305b7ced1c0d10970da58b6151030c314a
AndresGutierrezHurtado <andres52885241@gmail.com>
2024-08-05 19:02:30 -0500

    pagination added in the admin section

--- src/models/product.js
+++ src/models/product.js
@@ -6,6 +6,7 @@ class Product {
     }
 
     getAll(req, res) {
+        // Obtener todos los productos
         this.conn.query(`SELECT * FROM products ORDER BY ${req.query.sort || 'product_id'} ${req.query.order || 'asc'} `, (err, result) => {
             if (err) {
                 console.error('Error en la consulta:', err);
@@ -20,31 +21,89 @@ class Product {
     }
 
     paginate(req, res) {
-        const { page = 1, limit = 5 } = req.query;
+        const { page = 1, limit = 5, search = "" } = req.query;
+    
+        // Obtener la cantidad de registros que coinciden con la búsqueda
+        const countQuery = `SELECT COUNT(*) AS count FROM products WHERE product_name LIKE ? or product_description LIKE ? or product_id LIKE ?`;
+        this.conn.query(countQuery, [`%${search}%`, `%${search}%`, `%${search}%`], (err, countResult) => {
 
-        this.conn.query(`SELECT * FROM products ORDER BY ${req.query.sort || 'product_id'} ${req.query.order || 'asc'} LIMIT ${(page - 1) * limit}, ${limit}`, (err, result) => {
             if (err) {
                 console.error('Error en la consulta:', err);
-                return res.status(500).send('Error en el servidor');
+                return res.status(500).send('Error en el servidor 1');
             }
-            if (!result || result.length === 0) {
-                console.warn('No se encontraron resultados');
-                return res.status(404).send('No se encontraron resultados');
-            }
-            result.page = page;
-            result.limit = limit;
-            return res.json(result);
+    
+            const totalCount = countResult[0].count;
+            const totalPages = Math.ceil(totalCount / limit);
+            
+            // Obtener los registros paginados
+            const query = `SELECT * FROM products WHERE product_name LIKE '%${search}%' or product_description LIKE '%${search}%' or product_id LIKE '%${search}%' ORDER BY ${req.query.sort || 'product_id'} ${req.query.order || 'asc'} LIMIT ${(page - 1) * limit}, ${limit}`;
+            this.conn.query(query, (err, result) => {
+                if (err) {
+                    console.error('Error en la consulta:', err);
+                    return res.status(500).send('Error en el servidor 2');
+                }
+                if (!result || result.length === 0) {
+                    console.warn('No se encontraron resultados');
+                    return res.status(404).send('No se encontraron resultados');
+                }
+                
+                // Enviar los resultados paginados junto con la información de paginación
+                res.json({
+                    products: result,
+                    page,
+                    limit,
+                    totalPages,
+                    totalCount
+                });
+            });
         });
-        
-    }
+    }   
     
     getById(req, res) {
+        // Obtener un solo producto
         this.conn.query('SELECT * FROM products WHERE product_id = ?', [req.params.id], (err, result) => {
             if(err) res.status(500).send('Error en el servidor');
             res.json(result); 
         });
     }
 
+    updateById(id, data, res) {
+        // Actualizar un solo producto
+        let query = 'UPDATE products SET ';
+        for (const key in data) {
+            query += `${key} = '${data[key]}', `;
+        }
+        query = query.slice(0, -2);
+        query += ' WHERE product_id = ' + id;
+
+        this.conn.query(query, [], (err, result) => {
+            if (err) {
+                console.error('Error en la consulta:', err);
+                return res.status(500).json({ success: false, message: 'Error en el servidor' });
+            }
+            if (result.affectedRows === 0) {
+                console.warn('No se encontró el producto para actualizar');
+                return res.status(404).json({ success: false, message: 'No se encontró el producto' });
+            }
+            return res.json({ success: true, message: 'Producto actualizado exitosamente' });
+        });
+    }
+
+    deleteById(id, res) {
+        // Eliminar un solo producto
+        this.conn.query('DELETE FROM products WHERE product_id = ?', [id], (err, result) => {
+            if (err) {
+                console.error('Error en la consulta:', err);
+                return res.status(500).json({ success: false, message: 'Error en el servidor' });
+            }
+            if (result.affectedRows === 0) {
+                console.warn('No se encontró el producto para eliminar');
+                return res.status(404).json({ success: false, message: 'No se encontró el producto' });
+            }
+            return res.json({ success: true, message: 'Producto eliminado exitosamente' });
+        });
+    }
+    
     generatePDF(req, res) {
         let products = [];
 
@@ -142,40 +201,6 @@ class Product {
         });
     }
 
-    updateById(id, data, res) {
-        let query = 'UPDATE products SET ';
-        for (const key in data) {
-            query += `${key} = '${data[key]}', `;
-        }
-        query = query.slice(0, -2);
-        query += ' WHERE product_id = ' + id;
-
-        this.conn.query(query, [], (err, result) => {
-            if (err) {
-                console.error('Error en la consulta:', err);
-                return res.status(500).json({ success: false, message: 'Error en el servidor' });
-            }
-            if (result.affectedRows === 0) {
-                console.warn('No se encontró el producto para actualizar');
-                return res.status(404).json({ success: false, message: 'No se encontró el producto' });
-            }
-            return res.json({ success: true, message: 'Producto actualizado exitosamente' });
-        });
-    }
-
-    deleteById(id, res) {
-        this.conn.query('DELETE FROM products WHERE product_id = ?', [id], (err, result) => {
-            if (err) {
-                console.error('Error en la consulta:', err);
-                return res.status(500).json({ success: false, message: 'Error en el servidor' });
-            }
-            if (result.affectedRows === 0) {
-                console.warn('No se encontró el producto para eliminar');
-                return res.status(404).json({ success: false, message: 'No se encontró el producto' });
-            }
-            return res.json({ success: true, message: 'Producto eliminado exitosamente' });
-        });
-    }
 }
 
 module.exports = Product;
