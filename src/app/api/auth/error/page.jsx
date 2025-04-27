1cc21a5c87951250bac4939871ba04d826501013
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-04-27 15:46:50 -0500

    feat: add metadata for authentication error page

--- src/app/api/auth/error/page.jsx
+++ src/app/api/auth/error/page.jsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import React from "react";
 
+export const metadata = {
+    title: "Error de autenticación",
+    description: "Hubo un error al autenticarte.",
+};
+
 export default async function Page({ searchParams }) {
     const { error } = await searchParams;
 
