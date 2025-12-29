ef2702bfda93b6eb541335b17e668d7d14a376c5
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-12-29 13:11:32 -0500

    feat: add Role and User entity classes with properties and methods for user management

---
+++ server/src/main/java/com/accomputers/api/domain/entities/Role.java
@@ -0,0 +1,29 @@
+package com.accomputers.api.domain.entities;
+
+public class Role {
+    private Integer id;
+    private String name;
+
+    public Role(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ server/src/main/java/com/accomputers/api/domain/entities/User.java
@@ -0,0 +1,78 @@
+package com.accomputers.api.domain.entities;
+
+public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Integer roleId;
+    private Role role;
+
+    public User(Integer id, String firstName, String lastName, String email, String password, Integer roleId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roleId = roleId;
+    }
+
+    // Getters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    // Setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
