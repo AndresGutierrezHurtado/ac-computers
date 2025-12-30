a7b987826c6591230883dd797b75596638e95b8c
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-12-30 14:28:55 -0500

    feat: add Spring Security configuration and dependencies to enhance application security

---
+++ server/src/main/java/com/accomputers/api/infrastructure/http/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.accomputers.api.infrastructure.http;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+// security filter chain
+@Configuration
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+}
