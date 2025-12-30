a5cbfa1acd568ad2d4c83f3c95fc526e8cb43dea
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-12-30 18:59:00 -0500

    feat: implement GlobalExceptionHandler for centralized exception handling in the API, providing standardized responses for various exceptions

---
+++ server/src/main/java/com/accomputers/api/infrastructure/http/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.accomputers.api.infrastructure.http;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// Domain
+import com.accomputers.api.domain.exceptions.EntityNotFoundException;
+import com.accomputers.api.domain.exceptions.InvalidValueObjectException;
+import com.accomputers.api.domain.exceptions.DomainException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ResponseDTO<Void>> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDTO<Void>(e.getMessage(), false));
+    }
+
+    @ExceptionHandler(InvalidValueObjectException.class)
+    public ResponseEntity<ResponseDTO<Void>> handleInvalidValueObjectException(InvalidValueObjectException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDTO<Void>(e.getMessage(), false));
+    }
+
+    @ExceptionHandler(DomainException.class)
+    public ResponseEntity<ResponseDTO<Void>> handleDomainException(DomainException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO<Void>(e.getMessage(), false));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseDTO<Void>> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO<Void>("Internal server error", false));
+    }
+}
