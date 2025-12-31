32a0ddc6c0295ecd080de08483fd1125ff66e4d2
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-12-31 13:53:44 -0500

    feat: add UserResponseDTO for standardized user response data transfer object

---
+++ server/src/main/java/com/accomputers/api/application/dtos/response/UserResponseDTO.java
@@ -0,0 +1,27 @@
+package com.accomputers.api.application.dtos.response;
+
+import com.accomputers.api.domain.entities.Role;
+import com.accomputers.api.domain.entities.User;
+
+public record UserResponseDTO(
+        Integer id,
+        String firstName,
+        String lastName,
+        String email,
+        Integer roleId,
+        Role role) {
+
+    public static UserResponseDTO fromUser(User user) {
+        if (user == null) {
+            return null;
+        }
+
+        return new UserResponseDTO(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail() != null ? user.getEmail().getValue() : null,
+                user.getRoleId(),
+                user.getRole());
+    }
+}
