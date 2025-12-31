6cec3ef8cc220b2b9456310f369d934487906d07
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-12-31 15:10:47 -0500

    feat: replace LangChain4j with Spring AI for chat functionality, updating AssistantService to use OllamaChatModel and modifying application properties for new configuration

--- server/src/main/java/com/accomputers/api/infrastructure/ai/AssistantService.java
+++ server/src/main/java/com/accomputers/api/infrastructure/ai/AssistantService.java
@@ -1,12 +1,28 @@
 package com.accomputers.api.infrastructure.ai;
 
-import dev.langchain4j.service.SystemMessage;
-import dev.langchain4j.service.UserMessage;
-import dev.langchain4j.service.spring.AiService;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
+import org.springframework.stereotype.Component;
 
-@AiService
-public interface AssistantService {
+@Component
+public class AssistantService {
+    private final OllamaChatModel chatModel;
 
-    @SystemMessage("Responde educado y breve")
-    public String chat(@UserMessage String userMessage);
+    public AssistantService() {
+        OllamaApi ollamaApi = OllamaApi.builder().build();
+
+        this.chatModel = OllamaChatModel.builder()
+                .ollamaApi(ollamaApi)
+                .defaultOptions(
+                        OllamaChatOptions.builder()
+                                .model("llama3.1:8b")
+                                .temperature(0.4)
+                                .build())
+                .build();
+    }
+
+    public String chat(String message) {
+        return this.chatModel.call(message);
+    }
 }
