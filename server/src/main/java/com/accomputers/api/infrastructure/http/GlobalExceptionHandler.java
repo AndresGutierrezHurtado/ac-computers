e107d6c15847046d5f067a30efece6e419b5bdf2
AndresGutierrezHurtado <andres52885241@gmail.com>
2026-01-06 18:34:00 -0500

    feat: update GlobalExceptionHandler to replace SQLException handling with DataAccessException, improving error management and adding specific foreign key constraint handling

--- server/src/main/java/com/accomputers/api/infrastructure/http/GlobalExceptionHandler.java
+++ server/src/main/java/com/accomputers/api/infrastructure/http/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.accomputers.api.infrastructure.http;
 
-import java.sql.SQLException;
-
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,7 +16,7 @@ import com.accomputers.api.infrastructure.http.responses.ResponseDTO;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ResponseDTO<Void>> handleEntityNotFoundException(EntityNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDTO<Void>(e.getMessage(), false));
@@ -30,18 +29,26 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<ResponseDTO<Void>> handleDomainException(DomainException e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO<Void>(e.getMessage(), false));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseDTO<Void>(e.getMessage(), false));
     }
 
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity<ResponseDTO<Void>> handleSQLException(SQLException e) {
-        System.out.println("SQLException: " + e.getMessage() + " Stack Trace: " + e.getStackTrace());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO<Void>("There was an error with the database, check the server logs for more information", false));
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ResponseDTO<Void>> handleDataAccessException(DataAccessException e) {
+        if (e.getMessage().toLowerCase().contains("cannot add or update a child row: a foreign key constraint fails")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDTO<Void>(
+                    "The entity cannot be manipulated because it has a foreign key constraint to another entity", false));
+        }
+
+        System.out.println("Database error: " + e.getMessage() + " Stack Trace: " + e.getStackTrace());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO<Void>(
+                "There was an error with the database, check the server logs for more information", false));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResponseDTO<Void>> handleException(Exception e) {
         System.out.println("Exception: " + e.getMessage() + " Stack Trace: " + e.getStackTrace());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDTO<Void>("Internal server error", false));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ResponseDTO<Void>("Internal server error", false));
     }
 }
