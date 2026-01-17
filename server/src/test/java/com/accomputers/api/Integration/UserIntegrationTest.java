efb714cf889368e6e7d3d2562e298107eee683f2
AndresGutierrezHurtado <andres52885241@gmail.com>
2026-01-16 21:29:57 -0500

    feat: add test for user registration to validate handling of invalid password

--- server/src/test/java/com/accomputers/api/Integration/UserIntegrationTest.java
+++ server/src/test/java/com/accomputers/api/Integration/UserIntegrationTest.java
@@ -61,4 +61,10 @@ public class UserIntegrationTest {
         RegisterDTO registerDTO = new RegisterDTO("John", "Doe", "invalid-email", "password", 1);
         assertThrows(InvalidValueObjectException.class, () -> authService.register(registerDTO));
     }
+
+    @Test
+    public void create_user_with_invalid_password() {
+        RegisterDTO registerDTO = new RegisterDTO("John", "Doe", "john.doe@example.com", "123", 1);
+        assertThrows(InvalidValueObjectException.class, () -> authService.register(registerDTO));
+    }
 }
